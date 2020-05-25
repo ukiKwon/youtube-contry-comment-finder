1a0ce59d92361fcefb906159a9978889542d0427
snrndi121 <snrndi4521@gmail.com>
2020-05-26 04:37:22 +0900

    feat : 댓글수 늘리기 위한 전초과정 -> 기능2부분) ) ( )

--- src/public/scripts/modules_op.js
+++ src/public/scripts/modules_op.js
@@ -1,16 +1,17 @@
-const api_key = "AIzaSyDvk8_0Ncc7vLvoWT4gfTxeFs5DLIorSZ0";
+const api_key = "AIzaSyBHY-BGzwAKRttO0uRbV20Xw7TO_vGO_YY";
 const maxResults = 1;
+const maxPage = 3;
 /*
 * 비디오 댓글 관련 structure
   cmtInfo = {"imgUrl" : , "userId:" , "comment:" }
-  cmtInfo1Arr = [cmtInfo]
-  videoCmtInfo = {"videoId":, "commentList:" cmtInfo1Arr }
+  cmtInfoArr = {"nextPageToken" :, "comment_count":, "commentList": [cmtInfo]}
+  videoCmtInfo = {"videoId":, "comment_count":, "korPercent": }
 
 * 재생목록 관련 structure
-  playItem = {"videoid":, title":, "korPercent":,}
-  playItem1Arr = [playItem]
-  playList = {"groupId": , "item" : playItem1Arr}
-  playList2Arr = [playList]
+  videoItem = {"videoid":, title":, "img":, "korPercent":,}
+  videoList = [videoItem]
+  playListOne = {"groupId": , "item" : videoList}
+  playListAll = {"channelId": , "playList" : playListOne}
 
 */
 function displayItem(target) {
@@ -36,9 +37,12 @@ function displayItem(target) {
         else reject("Err : display source is empty");
     })
 }
-function getVideoComment(vids) {
+function getVideoComment(_nextpage) {
     return new Promise(function(resolve, reject) {
-        var url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key="+api_key+"&videoId="+vids+"&maxResults="+maxResults;
+        const videoId = $("#videoId").val();
+        let url = "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&key="+api_key+"&videoId="+vids+"&maxResults="+maxResults;
+        if (_nextpage != "") url += ("&pageToken=" + _nextpage);
+
         $.get(url, function(data) {
             if (data) resolve(data);
             else reject("Err : video info or url is wrong");
@@ -47,30 +51,37 @@ function getVideoComment(vids) {
 }
 function getBriefComment(data) {
     return new Promise(function(resolve, reject) {
-        var cmtInfo1Arr = new Array();
-        for(var i=0; i<data.items.length; ++i) {
-            var cmtInfo = new Object();
+        if (!data) {reject("Err : getImgTitleText() has no data");}
+        let cmtInfoArr = new Object();
+        let i = 0;
+        cmtInfoArr.commentList = new Array();
+        for(; i<data.items.length; ++i) {
+            let cmtInfo = new Object();
             cmtInfo.imgUrl = data.items[i].snippet.topLevelComment.snippet.authorProfileImageUrl;
             cmtInfo.userid = data.items[i].snippet.topLevelComment.snippet.authorDisplayName;
             cmtInfo.comment = data.items[i].snippet.topLevelComment.snippet.textDisplay;
-            cmtInfo1Arr.push(cmtInfo);
+            cmtInfoArr.commentList.push(cmtInfo);
         }
-        if (cmtInfo1Arr) { resolve(cmtInfo1Arr);}
-        if (!data) {reject("Err : getImgTitleText() has no data");}
+
+        if (i == data.items.length) {
+          cmtInfoArr.comment_count = data.items.length;
+          cmtInfoArr.nextPageToken = data.nextPageToken;
+          resolve(cmtInfoArr);
+        }
     })
 }
-function getKorPercent(_videoCmt1Arr) {
+function getKorPercent(_cmtInfoArr) {
     return new Promise(function(resolve, reject) {
-        if (_videoCmt1Arr.commentList.length == 0) {
+        if (_cmtInfoArr.commentList.length == 0) {
             reject("댓글을 먼저 불러와주세요");
         }
-        var i = 0, total = _videoCmt1Arr.commentList.length;
-        var count = 0, res = 0;
-        var commentlist = _videoCmt1Arr.commentList;
+        let i = 0, total = _cmtInfoArr.commentList.length;
+        let count = 0, res = 0;
+        let commentlist = _cmtInfoArr.commentList;
         for (i=0; i<commentlist.length; ++i) {
               //1. 하이퍼 링크 제거
-              var tar_str = commentlist[i].comment;
-              var find_index = tar_str.lastIndexOf("</a>");
+              let tar_str = commentlist[i].comment;
+              let find_index = tar_str.lastIndexOf("</a>");
               if (find_index != -1) {//하이퍼링크가 있다면
                   tar_str = tar_str.substr(find_index+5, tar_str.length-find_index+4);
               }
@@ -93,9 +104,9 @@ function getKorPercent(_videoCmt1Arr) {
     })
 }
 function getPlaylistData() {
-    var videoId = $("#videoId").val();
-    var url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&key="+api_key+"&channelId="+videoId+"&maxResults="+maxResults;
     return new Promise(function(resolve, reject) {
+        let videoId = $("#videoId").val();
+        const url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&key="+api_key+"&channelId="+videoId+"&maxResults="+maxResults;
         $.get(url, function(data) {//대표 재생목록 가져오기
             if (data) {resolve(data);}
             else reject("Err : No playlist found!");
@@ -104,15 +115,15 @@ function getPlaylistData() {
 }
 function getBriefPlayList(data) {
     return new Promise(function(resolve, reject) {
-        var briefPL = new Array();//대표재생목록
-        var result = "";
-        var i = 0;
+        let videoList = new Array();//대표재생목록
+        let result = "";
+        let i = 0;
         for(i=0; i<data.items.length; ++i) {
-            var rJson = new Object();
-            rJson.id = data.items[i].id;
+            let videoItem = new Object();
+            videoItem.videoId = data.items[i].id;
             // rJson.title = data.items[i].snippet.title;
             // rJson.img = data.items[i].snippet.thumbnails.medium.url;
-            briefPL.push(rJson);
+            videoList.push(videoItem);
             result = `
                 <div class="well">
                     <img class="img-rounded" src="${data.items[i].snippet.thumbnails.medium.url}">
@@ -126,58 +137,66 @@ function getBriefPlayList(data) {
             $("#results").append(result);
         }
         if (i == data.items.length) {
-            resolve(briefPL);
+            resolve(videoList);
         } else {
             reject("Err : getBriefPlayList() is not working");
         }
     })
 }
-function getPlaylistItemData(playlist_gid) {
-    return new Promise(function(resovle, reject) {
-        var playlistItem = new Array();//소속 재생목록
-
-        for (var i=0; i<playlist_gid.length; ++i) {
-           var url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key="+api_key+"&playlistId="+playlist_gid[i].id+"&maxResults="+maxResults;
-           $.get(url, function(data){
-               for (var j=0; j<data.items.length; ++j) {
-                   var vJson = new Object();
-                   vJson.groupId = playlist[i].id;
-                   vJson.videoid = data.items[i].resourceId.videoId;
-                   vJson.title = data.items[i].snippet.title;
-                   playlistItem.push(vJson);
-               }
-           })
-        }
-        if (!playlistItem) resolve(playlistItem);
-        else reject("Err : no playlist item were found");
+function getPlaylistItem(_videoList) {//한 비디오 항목에 대한 재생목록 리스트를 담아야함
+    return new Promise(function(resolve, reject) {
+          if (!_videoList) reject("Err : getPlaylistItem()-> no playlist item were found");
+          let i = 0;
+          console.log(_videoList);
+          let playListAll = new Array();//대표 비디오-비디오 목록
+          for (; i < _videoList.length; ++i) {
+              const url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key="+api_key+"&playlistId="+_videoList[i].videoId+"&maxResults="+maxResults;
+              let playListOne = new Object();
+              let videoList = new Array();
+               $.get(url, function(data) {
+                     for (var j=0; j<data.items.length; ++j) {
+                         let videoItem = new Object();
+                         videoItem.videoid = data.items[i].snippet.resourceId.videoId;
+                         videoItem.title = data.items[i].snippet.title;
+                         videoItem.img = "";
+                         videoItem.korPercent = -1;
+                         videoList.push(videoItem);
+                     }
+                     if (j == data.items.length) {
+                          playListOne.groupId = _videoList[i].videoId;
+                          playListOne.item = videoList;
+                          playListAll.push(playListOne);
+                     }
+                     if (data && !playListAll.channelId) { playListAll.channelId = data[0].items[0].snippet.channelId;}
+               })
+           }
+           if (i == _videoList.length) { resolve(playListAll);}
     })
 }
 
 $(document).ready(function() {
-    let cmtInfo1Arr = new Object();
-    let videoCmtInfo = new Object();
+    let cmtInfoArr = new Object();//
 
     //기능1. 단일 비디오 내 댓글 가져오기
     $('#get_v_cmt').click(function(e) {
         e.preventDefault();
         $("#results").empty();
         var result = "";
-        var videoId = $("#videoId").val();
-        getVideoComment(videoId).then(function(raw_cmtInfo) { return getBriefComment(raw_cmtInfo);})
-        .then(function(cmtInfoArr) {
-              videoCmtInfo.videoId = videoId;
-              videoCmtInfo.commentList = cmtInfoArr;
-
-              for (var i=0; i < cmtInfoArr.length; ++i) {
+        getVideoComment("").then(function(_raw_cmtInfo) { return getBriefComment(_raw_cmtInfo);})
+        .then(function(_cmtInfoArr) {
+              // videoCmtInfo.videoId = videoId;
+              // videoCmtInfo.commentList = _cmtInfoArr.commentList;
+              cmtInfoArr = _cmtInfoArr;
+              for (var i=0; i < _cmtInfoArr.commentList.length; ++i) {
                     result = `
                     <div class="well">
-                    <img class="img-rounded" src="${cmtInfoArr[i].imgUrl}">
+                    <img class="img-rounded" src="${_cmtInfoArr.commentList[i].imgUrl}">
                     </div>
 
-                    <h5>${cmtInfoArr[i].userId}</h5>
+                    <h5>${_cmtInfoArr.commentList[i].userId}</h5>
 
                     <p>
-                      ${cmtInfoArr[i].comment}
+                      ${_cmtInfoArr.commentList[i].comment}
                     </p>
                     `;
                     // <a href="${data.item[i].snippet.topLevelComment.snippet.authorChannelUrl}" target="_blank">
@@ -206,27 +225,45 @@ $(document).ready(function() {
     })
     //기능2. 단일 비디오 내 한국인 댓글 판단
     $('#get_kor_pc').click(function(e) {
-        getKorPercent(videoCmtInfo).then(function(korpercent) { alert("> korean percent : " + korpercent + "%");})
-        .catch(function(err) { alert(err); })
+        let videoCmtInfo = new Object();//기능1 : 하나의 비디오에 대한 댓글 정보
+        //특정 비디오 들어오면
+        videoCmtInfo.videoId = $("#videoId").val();
+        videoCmtInfo.comment_count = 0;
+        videoCmtInfo.korPercent = 0;
+        let nextpage="";
+        //  videoCmtInfo = {"videoId":, "comment_count":, "korPercent": }
+        for (var page=0; page<maxPage; ++page) {
+            getVideoComment(nextpage)
+            .then(function(val) { return getBriefComment(val);})
+            .then(function(comment){
+                videoCmtInfo.comment_count += comment.comment_count;
+                return getKorPercent(comment);})
+            .then(function(korpercent){
+                new Promise function(resolve, reject ) {(
+
+                  if (videoCmtInfo.korPercent != 0) {
+                    videoCmtInfo.korPercent += korpercent;
+                    resolve("videoCmtInfo.korPercent");
+
+                    videoCmtInfo.korPercent /2;
+                    videoCmtInfo.korPercent = korpercent; insert(kichend 남세);}
+
+               )//한번이라도 계산된적 있다면
+        }
     })
+
     //기능3. 채널 내 외국인이 가장 많이 사랑한 영상
     $('#get_best_glob').click(function(e) {
         e.preventDefault();
         $("#results").empty();
         getPlaylistData()
-        .then(function(playlist) {return getBriefPlayList(playlist);})//playlist 받기
-        .then(function(playlist_groupid) {
-            // new Promise(function(resolve, reject) {
-            //     for (var i = 0; i < playlist_groupid.length; ++i) {//각 playlist에 대하여
-            //       videoCmt2Arr =
-            //     }
-            //
-            // })
-            return getPlaylistItemData(playlist_groupid);
-        })
-        .then(function(playlistItem) {return getVideoComment(playlistItem);})// 댓글 가져오고
-        .then(function(videocmts) { return getKorPercent(videocmts);})// 한글판별
+        .then(function(videoList_raw) { return getBriefPlayList(videoList_raw);})
+        .then(function(videoList) { return getPlaylistItem(videoList);})//대표 비디오에 대하여 비디오 목록 불러오기
         .catch(function(err) { console.log(err);})
+        .then(function(playListAll) { return getVideoComment(playListAll);})// 댓글 가져오고
+        .catch(function(err) { console.log(err);});
+        // .then(function(videocmts) { return getKorPercent(videocmts);})// 한글판별
+        // .catch(function(err) { console.log(err);})
         // 비디오 이름 + 조회수 + 댓글 퍼센트 기록
         //댓글 퍼센트 중심으로 정렬
         //테이블 형태로 보여주기
